b452af3eddd5ed755bd4eb5993514e1344025905
Emilia Zysk <emzysk.dev@gmail.com>
2023-12-02 23:53:35 +0900

    words

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import words from "./wordList.json";
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  console.log("wordToGuess :>> ", wordToGuess);
+
   return <h1>HI</h1>;
 }
 
